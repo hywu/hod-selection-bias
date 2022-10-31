02558c647c45d7423933fb22c99567d45da17517
Heidi Wu <hwu@borah-login.cm.cluster>
2022-10-31 16:14:20 -0600

    updated pmem

--- repo/utils/pmem_weights.py
+++ repo/utils/pmem_weights.py
@@ -5,7 +5,7 @@ from scipy.interpolate import RegularGridInterpolator
 Pmem_2D_file = 'data/2d_RovRLambda_pmem_map_L15_z0p5.txt'
 Pmem_2d_data = np.genfromtxt(Pmem_2D_file)
 
-delz_bins = np.linspace(-0.15, 0.15, 16) #Of course we can change this, but it's what I have computed so far
+delz_bins = np.linspace(-0.15, 0.15, 16)
 R_bins    = np.linspace(0.0, 1.0, 8)
 
 
@@ -14,16 +14,22 @@ R_vals    = (R_bins[1:] + R_bins[:-1])/2.0
 
 pmem_interp = RegularGridInterpolator((delz_vals, R_vals), Pmem_2d_data)
 
-def pmem_weights(dz, R):
+Rmin = min(R_vals)
+Rmax = max(R_vals)
+
+zmin = min(delz_vals)
+zmax = max(delz_vals)
+
+dz_min = zmin
+def pmem_weights(dz, R, dz_max=zmax):
     pmem_list = np.zeros(len(dz))
     
     # need to restrict to interpolated region
-    sel = (dz >= min(delz_vals))&(dz <= max(delz_vals))&(R >= min(R_vals))&(R <= max(R_vals))
+    sel = (dz >= dz_min)&(dz <= dz_max)&(R >= Rmin)&(R <= Rmax)
     pmem_list[sel] = pmem_interp((dz[sel], R[sel]))
     
-    # very small R => set pmem = 1
-    sel2 = (dz > min(delz_vals))&(dz < max(delz_vals))&(R < min(R_vals))
-    pmem_list[sel2] = 1
-    
-    return pmem_list
+    # very small R => set pmem to the smallest R value
+    sel2 = (dz >= dz_min)&(dz <= dz_max)&(R < Rmin)
+    pmem_list[sel2] = pmem_interp((dz[sel2], R[sel2]*0 + Rmin))
 
+    return pmem_list
