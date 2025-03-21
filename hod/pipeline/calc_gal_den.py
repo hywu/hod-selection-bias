31b46204630dfc6d08d5ec1d405cf048e3d6fb4a
Wu <hywu@c001.cm.cluster>
2025-03-21 10:56:32 -0500

    add calc gal density

---
+++ hod/pipeline/calc_gal_den.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+import fitsio
+#import matplotlib.pyplot as plt
+import numpy as np
+import os
+import sys
+import yaml
+from hod.utils.read_sim import read_sim
+
+
+
+class CalcGalDen(object):
+    def __init__(self, yml_fname):
+
+        with open(yml_fname, 'r') as stream:
+            try:
+                self.para = yaml.safe_load(stream)
+            except yaml.YAMLError as exc:
+                print(exc)
+
+        self.depth = self.para['depth']
+        perc = self.para['perc']
+        
+        #### For AbacusSummit ####
+        if self.para['nbody'] == 'abacus_summit':
+            cosmo_id = self.para.get('cosmo_id', None)
+            hod_id = self.para.get('hod_id', None)
+            phase = self.para.get('phase', None)
+            redshift = self.para['redshift']
+            if redshift == 0.3: z_str = '0p300'
+            output_loc = self.para['output_loc']+f'/base_c{cosmo_id:0>3d}_ph{phase:0>3d}/z{z_str}/'
+        else:
+           output_loc = self.para['output_loc']
+
+        #output_loc = self.para['output_loc']
+        model_name = self.para['model_name']
+
+        self.rich_name = self.para['rich_name'] #+ f'{self.depth}'
+        self.out_path = f'{output_loc}/model_{model_name}'
+        redshift = self.para['redshift']
+        self.survey = self.para.get('survey', 'desy1')
+        
+        self.obs_path = f'{self.out_path}/obs_{self.rich_name}_{self.survey}/'
+        #print('obs_path', self.obs_path)
+        if os.path.isdir(self.obs_path)==False: 
+            os.makedirs(self.obs_path)
+
+        self.readcat = read_sim(self.para)
+
+        #self.mpart = self.readcat.mpart
+        self.boxsize = self.readcat.boxsize
+        #self.hubble = self.readcat.hubble
+        self.vol = self.boxsize**3
+
+    def calc_gal_den(self):
+        # calculate galaxy density
+        den_fname = f'{self.out_path}/gal_density.dat'
+        if os.path.exists(den_fname) == False:
+            # read in galaxies
+            gal_cat_format = self.para.get('gal_cat_format', 'fits')
+            if gal_cat_format == 'fits':
+                gal_fname = f'{self.out_path}/gals.fit'
+                data, header = fitsio.read(gal_fname, header=True)
+                x_gal_in = data['px']
+            
+            # if gal_cat_format == 'h5':
+            #     import h5py
+            #     loc = '/bsuhome/hwu/scratch/abacus_summit/'
+            #     gal_fname = loc + 'NHOD_0.10_11.7_11.7_12.9_1.00_0.0_0.0_1.0_1.0_0.0_c000_ph000_z0p300.hdf5'
+            #     f = h5py.File(gal_fname,'r')
+            #     data = f['particles']
+            #     #print(data.dtype)
+            #     x_gal_in = data['x']
+
+            ngal = len(x_gal_in)/self.vol
+            data = np.array([ngal]).transpose()
+            np.savetxt(den_fname, data, fmt='%-12g', header='ngal (h^3 Mpc^-3)')
+        else:
+            print('done')
+        print('file saved', den_fname)
+
+if __name__ == "__main__":
+    yml_fname = sys.argv[1]
+    cgd = CalcGalDen(yml_fname)
+    cgd.calc_gal_den()
+
---
+++ hod/pipeline/plot_abundance.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+import fitsio
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import sys
+import yaml
+from hod.utils.read_sim import read_sim
+from astropy.cosmology import FlatLambdaCDM
+
+#sys.path.append('../utils')
+
+#./plot_abundance.py ../yml/mini_uchuu/mini_uchuu_fid_hod.yml
+#./plot_abundance.py ../yml/abacus_summit/abacus_summit_template.yml
+
+#### comoving density * comoving volume => observed counts
+
+class PlotAbundance(object):
+    def __init__(self, yml_fname, zmin, zmax, survey_area_sq_deg):
+
+        with open(yml_fname, 'r') as stream:
+            try:
+                self.para = yaml.safe_load(stream)
+            except yaml.YAMLError as exc:
+                print(exc)
+
+        self.depth = self.para['depth']
+        perc = self.para['perc']
+        
+        #### For AbacusSummit ####
+        if self.para['nbody'] == 'abacus_summit':
+            cosmo_id = self.para.get('cosmo_id', None)
+            hod_id = self.para.get('hod_id', None)
+            phase = self.para.get('phase', None)
+            redshift = self.para['redshift']
+            if redshift == 0.3: z_str = '0p300'
+            output_loc = self.para['output_loc']+f'/base_c{cosmo_id:0>3d}_ph{phase:0>3d}/z{z_str}/'
+            from hod.utils.get_para_abacus_summit import get_cosmo_para
+            cosmo_abacus = get_cosmo_para(cosmo_id)
+        else:
+           output_loc = self.para['output_loc']
+
+        #output_loc = self.para['output_loc']
+        model_name = self.para['model_name']
+
+        self.rich_name = self.para['rich_name'] #+ f'{self.depth}'
+        self.out_path = f'{output_loc}/model_{model_name}'
+        redshift = self.para['redshift']
+        self.survey = self.para.get('survey', 'desy1')
+        self.obs_path = f'{self.out_path}/obs_{self.rich_name}_{self.survey}/'
+        if os.path.isdir(self.obs_path)==False: 
+            os.makedirs(self.obs_path)
+
+        self.readcat = read_sim(self.para)
+
+
+        self.boxsize = self.readcat.boxsize
+        
+        self.sim_vol = self.boxsize**3
+        #print('sim_vol', self.sim_vol)
+
+        self.ofname = f'{self.obs_path}/abundance.dat'
+        
+        fsky = survey_area_sq_deg/41253.
+        h = cosmo_abacus['hubble']
+        cosmo = FlatLambdaCDM(H0=h*100, Om0=cosmo_abacus['OmegaM'])
+        
+        self.survey_vol = fsky * 4. * np.pi/3. * (cosmo.comoving_distance(zmax).value**3 - cosmo.comoving_distance(zmin).value**3) * h**3  # (h/Mpc)**3
+        #print('sim_vol', self.sim_vol)
+        #print('survey_vol', self.survey_vol)
+
+
+    def calc_abundance(self, rich_fname=None): # allow intput fits file name directly
+        lambda_min_list = [ 20, 30, 45, 60]
+        lambda_max_list = [ 30, 45, 60, 1000]
+
+        if rich_fname == None:
+            rich_fname = f'{self.out_path}/richness_{self.rich_name}.fit'
+
+        data = fitsio.read(rich_fname)
+        lam = data['lambda']
+
+        counts_list = []
+        for ilam in range(len(lambda_min_list)):
+            sel = (lam >= lambda_min_list[ilam]) * (lam < lambda_max_list[ilam])
+            counts_list.append(len(lam[sel]) / self.sim_vol * self.survey_vol)
+
+        data = np.array([lambda_min_list, lambda_max_list, counts_list]).transpose()
+        np.savetxt(self.ofname, data, fmt='%-12g', header='lam_min, lam_max, counts')
+        #print('counts = ', counts_list)
+
+if __name__ == "__main__":
+    yml_fname = sys.argv[1]
+    ccr = PlotAbundance(yml_fname, zmin=0.2, zmax=0.35, survey_area_sq_deg=1437)
+    ccr.calc_abundance()
+
