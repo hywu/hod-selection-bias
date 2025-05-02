fe99af465580fdd7dfb998160111714d4089b61a
Heidi on M3 <hywu@smu.edu>
2025-05-02 08:15:57 -0500

    scripts for mcmc

---
+++ repo/mcmc/plot_mcmc.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use('MNRAS')
+import pandas as pd
+import os, sys
+import emcee
+from chainconsumer import Chain, ChainConsumer, ChainConfig
+from get_model import GetModel
+
+# ./plot_mcmc_results.ph s8Omhod wide abacus_summit 0 0 
+para_name = sys.argv[1] #'s8Omhod'
+emu_name = sys.argv[2] #'wide' # 'narrow'
+data_name = sys.argv[3] #'abacus_summit' #'flamingo'
+iz = int(sys.argv[4])
+run_id = int(sys.argv[5])
+
+z_list = [0.3, 0.4, 0.5]
+redshift = z_list[iz]
+
+# Parse the Yaml file
+yml_name = f'yml/emcee_{para_name}_{data_name}.yml'
+from parse_yml import ParseYml
+parse = ParseYml(yml_name)
+nsteps, nwalkers, lsteps, burnin, params_free_name, params_free_ini, params_range,\
+        params_fixed_name, params_fixed_value = parse.parse_yml()
+
+
+
+out_loc = f'/projects/hywu/cluster_sims/cluster_finding/data/emulator_mcmc/{emu_name}/mcmc_{data_name}/'
+plot_loc = f'../../plots/emulator/{emu_name}/{data_name}/'
+
+out_file = f'{out_loc}/mcmc_{para_name}_z{redshift}_run{run_id}.h5'
+print('output: ', out_file)
+
+reader = emcee.backends.HDFBackend(out_file, read_only=True)
+
+
+
+from chainconsumer import Chain, ChainConsumer, Truth, PlotConfig
+chain = Chain.from_emcee(reader, columns=parse.params_free_name, 
+                         name=f'{emu_name} {data_name}', color="blue", show_label_in_legend=True)
+c = ChainConsumer()
+c.add_chain(chain)
+# example
+# https://samreay.github.io/ChainConsumer/generated/gallery/advanced_examples/plot_5_multiple_truth_values/
+
+
+#### plot the truth # set line wideth manually in the end
+loc = dict(zip(parse.params_free_name, parse.params_free_truth))
+c.add_truth(Truth(location=loc, color='k'))#, linestyle="-", linewidth=20))
+
+#### plot the prior ranges
+para_min = dict(zip(parse.params_free_name, parse.params_free_range[:,0]))
+para_max = dict(zip(parse.params_free_name, parse.params_free_range[:,1]))
+c.add_truth(Truth(location=para_min, color='C0'))#, linestyle=":", linewidth=0.5))
+c.add_truth(Truth(location=para_max, color='C0'))#, linestyle=":", linewidth=0.5))
+
+#### latex label, fontsize
+lab = dict(zip(parse.params_free_name, parse.params_free_label))
+pc = PlotConfig(
+        labels=lab,
+        label_font_size = 30,
+        summary_font_size = 20,
+        tick_font_size = 15,
+        #tick_font = 'roman'
+)
+c.set_plot_config(pc)
+c.set_override(ChainConfig(shade_alpha=0.5))
+fig = c.plotter.plot()
+
+#### Plot the emulator range
+loc = '/projects/hywu/cluster_sims/cluster_finding/data/'
+train_loc = loc + f'emulator_train/{emu_name}/z0p{3+iz}00/'
+df = pd.read_csv(f'{train_loc}/parameters.csv')
+df = df[parse.params_free_name]
+
+
+ndim = len(parse.params_free_name)
+data = df.to_numpy()
+for i in range(0, ndim):
+    for j in range(0, ndim):
+        ax = plt.subplot(ndim, ndim,1+ndim*j+i)
+        if j > i:
+            ax.scatter(data[:, i], data[:, j], s=5, alpha=1, color='k')
+        if True:
+            # also set up plotting style
+            ax.grid(False)
+            for line in ax.lines:
+                #if line.get_linestyle() == "--":
+                line.set_linewidth(2)
+                
+plt.suptitle(f'{emu_name} {data_name}')
+
+plt.savefig(plot_loc+f'mcmc_{para_name}_z{redshift}_run{run_id}.pdf', dpi=72)
+
+###################################################################
+#### Plot posterior prediction
+# Sample 5000 points from the chain
+# Use emulator to calculate the posterior
+flat_samples = reader.get_chain(discard=100, thin=10, flat=True)
+print('after thinning', flat_samples.shape)
+nsamples, ndim = flat_samples.shape
+nsub = 500
+idx = np.random.randint(0, nsamples, size=nsub)
+subsample = flat_samples[idx]
+
+gm = GetModel(emu_name, iz, params_free_name, params_fixed_value, params_fixed_name)
+
+data_vec = np.loadtxt(f'../emulator/data_vector_{data_name}/data_vector_z{redshift}.dat')
+cov = np.loadtxt(f'../emulator/data_vector_abacus_summit/cov_z{redshift}.dat')
+
+plt.figure(figsize=(14,7))
+#### counts
+plt.subplot(1,2,1)
+x = np.arange(4)
+sigma = np.sqrt(np.diag(cov))
+plt.errorbar(x, data_vec[0:4], sigma[0:4], c='k', marker='o', mec='k', ls='', capsize=8)
+for i in range(nsub):
+    plt.plot(x, gm.model(subsample[i])[0:4], c='gray', alpha=0.01)
+plt.xlim(-0.1,3.1)
+plt.yscale('log')
+plt.ylabel('Counts')
+plt.xlabel('richness bin')
+plt.title(f' {emu_name} {data_name} z={redshift}')
+
+#### lensing
+plt.subplot(1,2,2)
+rp = np.loadtxt(train_loc+f'rp_rad.dat')
+nrp = len(rp)
+lensing_vec = data_vec[4:]
+lensing_sigma = sigma[4:]
+
+for ibin in range(4):
+    plt.errorbar(rp, rp*lensing_vec[ibin*nrp:(ibin+1)*nrp], \
+        rp*lensing_sigma[ibin*nrp:(ibin+1)*nrp],\
+        c=f'C{ibin}', marker='o', mec=f'C{ibin}', ls='', capsize=8)
+
+for i in range(nsub):
+    lensing_model = gm.model(subsample[i])[4:]
+    for ibin in range(4):
+        plt.plot(rp, rp*lensing_model[ibin*nrp:(ibin+1)*nrp], c=f'C{ibin}', alpha=0.01)    
+plt.xlim(0.9*min(rp), 1.1*max(rp))
+plt.xscale('log')
+plt.xlabel(r'$r_{\rm p}~[{\rm pMpc}]$')
+plt.ylabel(r'$r_{\rm p} \Delta\Sigma~[{\rm pMpc ~M_\odot/pc^2} ]$')
+plt.savefig(plot_loc+f'pred_{para_name}_z{redshift}_run{run_id}.pdf', dpi=72)
---
+++ repo/mcmc/run_mcmc.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use('MNRAS')
+import os, sys
+import emcee
+from get_model import GetModel
+
+#./run_mcmc.py s8Omhod wide abacus_summit 0 0
+
+para_name = sys.argv[1] #'s8Omhod'
+emu_name = sys.argv[2] #'wide' # 'narrow'
+data_name = sys.argv[3] #'abacus_summit' #'flamingo'
+iz = int(sys.argv[4])
+run_id = int(sys.argv[5])
+
+z_list = [0.3, 0.4, 0.5]
+redshift = z_list[iz]
+
+# Parse the Yaml file
+yml_name = f'yml/emcee_{para_name}_{data_name}.yml'
+from parse_yml import ParseYml
+parse = ParseYml(yml_name)
+nsteps, nwalkers, lsteps, burnin, params_free_name, params_free_ini, params_range,\
+        params_fixed_name, params_fixed_value = parse.parse_yml()
+
+out_loc = f'/projects/hywu/cluster_sims/cluster_finding/data/emulator_mcmc/{emu_name}/mcmc_{data_name}/'
+plot_loc = f'../../plots/emulator/{emu_name}/{data_name}/'
+if os.path.isdir(out_loc) == False:
+    os.makedirs(out_loc)
+if os.path.isdir(plot_loc) == False:
+    os.makedirs(plot_loc)
+
+out_file = f'{out_loc}/mcmc_{para_name}_z{redshift}_run{run_id}.h5'
+print('output: ', out_file)
+
+# save a back-up parameter file
+import yaml
+with open(yml_name, 'r') as stream:
+    try:
+        para = yaml.safe_load(stream)
+    except yaml.YAMLError as exc:
+        print(exc)
+
+with open(f'{out_loc}/para_{para_name}_z{redshift}_run{run_id}.yml', 'w') as outfile:
+    yaml.dump(para, outfile)
+
+
+if __name__ == "__main__":
+    #### Get the data
+    data_vec = np.loadtxt(f'../emulator/data_vector_{data_name}/data_vector_z{redshift}.dat')
+    data_cov = np.loadtxt(f'../emulator/data_vector_abacus_summit/cov_z{redshift}.dat')
+
+    #### Define the likelihood
+    from emcee_tools import LnLikelihood, runmcmc
+    gm = GetModel(emu_name, iz, params_free_name, params_fixed_value, params_fixed_name)
+    lnlike = LnLikelihood(data_vec, data_cov, gm.model, params_range,
+                                 params_free_name, params_fixed_value, params_fixed_name)
+
+    #### Run MCMC
+    pool=None
+
+    mcmc_chain, posterior = runmcmc(params_free_ini, nsteps, nwalkers, lsteps, 
+                                       lnlike.lnposterior, out_file,
+                                       pool, burnin=burnin)
+
+    '''
+    # moved to plot_mcmc.py
+    #### Plot posterior distribution
+    ndim = params_free_ini.size
+    fig, axes = plt.subplots(ndim, figsize=(10, 7), sharex=True)
+    reader = emcee.backends.HDFBackend(out_file, read_only=True)
+    samples = reader.get_chain()
+    print('np.shape(samples) =', np.shape(samples))
+    labels = parse.params_free_label
+    '''
+    '''
+    for i in range(ndim):
+        ax = axes[i]
+        ax.plot(samples[:, :, i], "k", alpha=0.3)
+        ax.set_xlim(0, len(samples))
+        ax.set_ylabel(labels[i])
+        ax.yaxis.set_label_coords(-0.1, 0.5)
+        
+    axes[-1].set_xlabel("step number");
+
+    plt.savefig(plot_loc+f'samples_{emu_name}_z0p{zid}00.pdf', dpi=72)
+    '''
+    '''
+    import corner
+    flat_samples = reader.get_chain(discard=100, thin=10, flat=True)
+    print('after thinning', flat_samples.shape)
+    truth = params_free_ini
+    fig = corner.corner(flat_samples, labels=labels)
+
+    if data_name == 'abacus_summit':
+        # add the truth (there must be a better way)
+        axes = np.array(fig.axes).reshape((ndim, ndim))
+        for i in range(ndim):
+            ax = axes[i, i]
+            ax.axvline(truth[i])
+
+    plt.savefig(plot_loc+f'mcmc_{para_name}_z{redshift}_run{run_id}.pdf', dpi=72)
+    print('plots saved at ' + plot_loc)
+    '''
+
