d96adc19694a2e3c82c7270afc18131df9cbe4ab
Heidi on M3 <hywu@smu.edu>
2025-05-19 09:42:23 -0500

    minor

--- hod/utils/merge_files.py
+++ hod/utils/merge_files.py
@@ -5,7 +5,7 @@ import pandas as pd
 from astropy.io import fits
 from astropy.table import Table
 
-def merge_files(in_fname='x*.dat', out_fname='x.fit', nfiles_expected=100):
+def merge_files(in_fname='x*.dat', out_fname='x.fit', nfiles_expected=100, delete=True):
     # merge all temp file and output a fits file with the same header
     fname_list = glob.glob(in_fname)
     nfiles = len(fname_list)
@@ -26,7 +26,8 @@ def merge_files(in_fname='x*.dat', out_fname='x.fit', nfiles_expected=100):
         hdul = fits.HDUList([primary_hdu, table_hdu])
         hdul.writeto(out_fname, overwrite=True)
 
-        os.system('rm -rf ' + in_fname)
+        if delete == True:
+            os.system('rm -rf ' + in_fname)
 
 if __name__ == '__main__':
     out_path = '/projects/hywu/cluster_sims/cluster_finding/data/emulator_data/base_c000_ph000/z0p300/model_hod000/'
