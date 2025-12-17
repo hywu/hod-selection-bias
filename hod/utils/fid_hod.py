f9e8ef0b70520e6efd2b03a8ee108b2a35d6aa98
Heidi on M3 <hywu@smu.edu>
2025-12-17 07:38:24 -0600

    add fcen

--- hod/utils/fid_hod.py
+++ hod/utils/fid_hod.py
@@ -13,22 +13,9 @@ def Ngal_S20_noscatt(M, alpha=1., lgM1=12.9, kappa=1., lgMcut=11.7, sigmalogM=0.
     x = (np.log10(M)-np.log10(Mcut))/sigmalogM
     Ncen = 0.5 * (1 + special.erf(x))
     y = (M - kappa * Mcut) / M1
-    y = np.maximum(y, 0)
+    y = np.maximum(y, 0) # enforce Nsat > 0 
     Nsat = Ncen * (y ** alpha)
-    #Nsat = max(0, Nsat)
     return Ncen, Nsat
-
-## TODO: merge with the 
-# def Ngal_S20_noscatt_arr(M, alpha=1., lgM1=12.9, kappa=1., lgMcut=11.7, sigmalogM=0.1):
-#     M = np.atleast_1d(M)
-#     Mcut = 10**lgMcut
-#     M1 = 10**lgM1
-#     x = (np.log10(M)-np.log10(Mcut))/sigmalogM
-#     Ncen = 0.5 * (1 + special.erf(x))
-#     y = (M - kappa * Mcut) / M1
-#     y[y < 0] = 0
-#     Nsat = Ncen * (y ** alpha)
-#     return Ncen, Nsat
 
 
 def Ngal_S20_poisson(M, alpha=1., lgM1=12.9, kappa=1., lgMcut=11.7, sigmalogM=0.1, sigmaintr=0, fcen=1.):
@@ -44,12 +31,10 @@ def Ngal_S20_poisson(M, alpha=1., lgM1=12.9, kappa=1., lgMcut=11.7, sigmalogM=0.
     y = (M - kappa * Mcut) / M1
     y = np.maximum(y, 0)
     Nsat_mean = Ncen * (y ** alpha) # determined by Ncen, not Ncen_incomp
-    #Nsat_mean = max(0, Nsat_mean)
     Nsat = poisson.rvs(Nsat_mean)
     if sigmaintr > 1e-8 and Nsat > 0:
         lnNsat = norm.rvs(np.log(Nsat), sigmaintr)
         Nsat = int(np.round(np.exp(lnNsat)))
-    #print('Ncen, Nsat', Ncen, Nsat)
     return Ncen_incomp, Nsat
 
 
